cab1f81da7d6065900aa4b6e0c004e6ea6717b47
Ittan Luna Sanchez <112011386+IttanLuna@users.noreply.github.com>
2025-03-26 09:12:04 -0400

    inicio del proyecto

---
+++ MicroMarket/MicroMarket/Contexto/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MicroMarket.Models;
+
+namespace MicroMarket.Contexto
+{
+    public class MyContext: DbContext
+    {
+        public MyContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Vendedor> Vendedores { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+    }
+}
