786e7466fead031219806ee044dda5c82c730d64
Ittan Luna Sanchez <112011386+IttanLuna@users.noreply.github.com>
2025-05-24 23:25:00 -0400

    revision de prueba

--- MicroMarket/MicroMarket/obj/Debug/net8.0/MicroMarket.AssemblyInfo.cs
+++ MicroMarket/MicroMarket/obj/Debug/net8.0/MicroMarket.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06d5c3e511f6c08615d9ebda2debf4da614f48b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad9315a3544f1ad52bad85700030ebc1007312c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
